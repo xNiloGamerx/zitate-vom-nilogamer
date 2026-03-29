73240035b8ff343e1e786cb421b1e5cf155c38c8
xNiloGamerx <nikb0322@gmail.com>
2026-03-29 19:59:53 +0200

    Changed Project structure

---
+++ electron/main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 480,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true
+    }
+  })
+
+  win.loadFile(path.join(__dirname, '../src/index.html'))
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  });
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+});
