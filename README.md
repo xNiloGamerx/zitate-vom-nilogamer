2b2d187effcf6b7ad4203273d86c6468be2c34e5
xNiloGamerx <nikb0322@gmail.com>
2026-04-04 18:27:27 +0200

    New Commit after cache clear

--- README.md
+++
@@ -1,2 +0,0 @@
-# Zitate vom Nilogamer
-Project for displaying funny inside joke quotes
--- update_quotes.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rsync -avz -e ssh cdelab:/home/quotes-zvm/zvm/quotes.json /home/kiosk-zvm/zitate-vom-nilogamer/assets/quotes.json
-rsync -avz -e ssh cdelab:/home/quotes-zvm/zvm/assets/user/icons /home/kiosk-zvm/zitate-vom-nilogamer/assets/user
