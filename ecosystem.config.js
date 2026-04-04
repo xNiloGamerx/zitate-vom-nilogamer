9c36da15a469c1867277f977ee896102957bbb0b
xNiloGamerx <nikb0322@gmail.com>
2026-04-04 22:30:08 +0200

    Updated ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,8 @@ module.exports = {
     args: "start",
     env: {
       DISPLAY: ":0",
-      XAUTHORITY: "/home/kiosk-zvm/.Xauthority" // Wichtig für die Rechte!
+      XAUTHORITY: "/home/kiosk-zvm/.Xauthority", // Wichtig für die Rechte!
+      DBUS_SESSION_BUS_ADDRESS: "unix:path=/run/user/$(id -u)/bus"
     }
   }]
 }
