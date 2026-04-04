e496babd40e553ef68e548467b953f30d0e9e48d
xNiloGamerx <nikb0322@gmail.com>
2026-04-04 22:47:31 +0200

    Update start-pm2 shell script for better perfomance by not starting npm

--- start-pm2.sh
+++ start-pm2.sh
@@ -2,4 +2,4 @@ MY_UID=$(id -u)
 
 MY_DBUS="unix:path=/run/user/$MY_UID/bus"
 
-DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=$MY_DBUS pm2 start npm --name "zvm" --env DISPLAY=:0 --env DBUS_SESSION_BUS_ADDRESS=$MY_DBUS -- start
+DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=$MY_DBUS pm2 start ./node_modules/.bin/electron --name "zvm" --env DISPLAY=:0 --env DBUS_SESSION_BUS_ADDRESS=$MY_DBUS -- .
