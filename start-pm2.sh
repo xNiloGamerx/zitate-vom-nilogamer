2bbf977c35e95c1c73ae06475ce5fe26ad7d1155
xNiloGamerx <nikb0322@gmail.com>
2026-04-04 22:34:13 +0200

    Updated start-pm2 shell script

--- start-pm2.sh
+++ start-pm2.sh
@@ -1,6 +1,7 @@
 DISPLAY=:0 \
 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u)/bus \
 pm2 start npm --name "zvm" \
---env DISPLAY=:0 \
---env DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u)/bus \
 -- start
+
+#--env DISPLAY=:0 \
+#--env DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u)/bus \
