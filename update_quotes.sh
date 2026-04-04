42dcd9a72282a9c90adbbfaf1c06541d6fed65c7
xNiloGamerx <nikb0322@gmail.com>
2026-04-04 22:18:02 +0200

    Updated update_quotes shell script

--- update_quotes.sh
+++ update_quotes.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rsync -avz -e ssh cdelab:/home/quotes-zvm/zvm/quotes.json /home/kiosk-zvm/zitate-vom-nilogamer/assets/quotes.json
-rsync -avz -e ssh cdelab:/home/quotes-zvm/zvm/assets/user/icons /home/kiosk-zvm/zitate-vom-nilogamer/assets/user
+# rsync -avz -e ssh cdelab:/home/quotes-zvm/zvm/quotes.json /home/kiosk-zvm/zitate-vom-nilogamer/assets/quotes.json
+rsync -avz -e ssh cdelab:/home/quotes-zvm/zvm/assets/ /home/kiosk-zvm/zitate-vom-nilogamer/assets
