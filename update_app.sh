bdcb15b9fcf85ecec7e7faf22bc183c50c523770
xNiloGamerx <nikb0322@gmail.com>
2026-04-05 21:46:04 +0200

    Updated update app shell script

--- update_app.sh
+++ update_app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /pfad/zu/deiner/app
+cd /home/kiosk-zvm/zitate-vom-nilogamer
 git fetch origin main
 
 # Prüfen, ob der lokale Stand hinter dem Remote liegt
@@ -10,6 +10,9 @@ REMOTE=$(git rev-parse "$UPSTREAM")
 if [ $LOCAL != $REMOTE ]; then
     echo "Änderungen gefunden. Aktualisiere..."
     git pull
+    chmod 744 ./start_pm2.sh
+    chmod 744 ./update_app.sh
+    chmod 744 ./update_quotes.sh
     # Hier den Befehl zum Neustart einfügen, z.B.:
     npm install
     pm2 reload zvm
