68986378d264dce6eebaafdda4ac9b50ac7aaa1e
xNiloGamerx <nikb0322@gmail.com>
2026-04-05 21:49:10 +0200

    Renamed start pm2 script for test, removed chmod because git update-index is set

--- update_app.sh
+++ update_app.sh
@@ -10,9 +10,6 @@ REMOTE=$(git rev-parse "$UPSTREAM")
 if [ $LOCAL != $REMOTE ]; then
     echo "Änderungen gefunden. Aktualisiere..."
     git pull
-    chmod 744 ./start_pm2.sh
-    chmod 744 ./update_app.sh
-    chmod 744 ./update_quotes.sh
     # Hier den Befehl zum Neustart einfügen, z.B.:
     npm install
     pm2 reload zvm
