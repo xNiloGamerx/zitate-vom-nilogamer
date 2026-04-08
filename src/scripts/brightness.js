44c793cee1b50c6ce470058166132bf240790861
xNiloGamerx <nikb0322@gmail.com>
2026-04-08 23:32:56 +0200

    Added brightness Setting

---
+++ src/scripts/brightness.js
@@ -0,0 +1,21 @@
+const brightnessSlider = document.getElementById('brightness-inp');
+const brightnessSliderLabel = document.getElementById('brightness-inp-label');
+
+function setBrightness(brightness) {
+  document.body.style.filter = `brightness(${brightness}%)`;
+}
+
+function updateSlider(min, max, value) {
+  // Berechnet den Prozentsatz für den Verlauf
+  const percentage = ((value - min) / (max - min)) * 100;
+  
+  brightnessSlider.style.backgroundSize = percentage + '% 100%';
+}
+
+brightnessSlider.addEventListener('input', (event) => {
+  updateSlider(event.target.min, event.target.max, event.target.value);
+  setBrightness(event.target.value);
+  brightnessSliderLabel.innerText = event.target.value;
+});
+
+updateSlider(brightnessSlider.min, brightnessSlider.max, brightnessSlider.value);
