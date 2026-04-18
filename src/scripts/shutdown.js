0b1aa9496bafc3829e50b919847c2f971b3c8928
xNiloGamerx <nikb0322@gmail.com>
2026-04-18 22:25:11 +0200

    Made shutdown button trigger shutdown

--- src/scripts/shutdown.js
+++ src/scripts/shutdown.js
@@ -1,3 +1,3 @@
 document.getElementById('settings-shutdown-btn').addEventListener('click', () => {
-  runPinInputModal(() => console.log("Yippie!"));
+  runPinInputModal(() => console.log(window.api.shutdownPi()));
 });
