8a9c42f6b7a1865f8e10e498c84e6c46f4b8c24e
xNiloGamerx <nikb0322@gmail.com>
2026-04-19 19:02:51 +0200

    Changed pm2 to absolute path

--- start_pm2.sh
+++ start_pm2.sh
@@ -5,4 +5,4 @@ MY_UID=$(id -u)
 
 MY_DBUS="unix:path=/run/user/$MY_UID/bus"
 
-DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=$MY_DBUS pm2 start ./node_modules/.bin/electron --name "zvm" --env DISPLAY=:0 --env DBUS_SESSION_BUS_ADDRESS=$MY_DBUS -- .
+DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=$MY_DBUS /home/kiosk-zvm/.nvm/versions/node/v25.9.0/bin/pm2 start ./node_modules/.bin/electron --name "zvm" --env DISPLAY=:0 --env DBUS_SESSION_BUS_ADDRESS=$MY_DBUS -- .
