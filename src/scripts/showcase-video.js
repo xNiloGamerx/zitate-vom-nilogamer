8ca379129fe0b2d2f0b1fe8fe4a673fe1aa092b0
xNiloGamerx <nikb0322@gmail.com>
2026-04-22 20:19:20 +0200

    Removed unneeded listeners

--- src/scripts/showcase-video.js
+++ src/scripts/showcase-video.js
@@ -2,16 +2,30 @@ const startShowcaseVideoBtn = document.getElementById('start-showcase-video-btn'
 const showcaseVideo = document.getElementById('showcase-video');
 const app = document.getElementById('app');
 
+function showShowcaseVideo() {
+  showcaseVideo.target.classList.add('display-none');
+  app.classList.add('display-none');
+}
+
+function hideShowcaseVideo() {
+  showcaseVideo.parentElement.classList.add('display-none');
+  app.classList.remove('display-none');
+}
+
+function handleStartShowcaseVideoBtn() {
+  showShowcaseVideo();
+  showcaseVideo.play();
+}
+
+function handleShowcaseVideoEnded() {
+  hideShowcaseVideo();
+  startShowcaseVideoBtn.removeEventListener('click', handleStartShowcaseVideoBtn);
+  showcaseVideo.removeEventListener('ended', handleShowcaseVideoEnded);
+}
+
 if (birthdayMode) {
-  startShowcaseVideoBtn.addEventListener('click', (event) => {
-    event.target.classList.add('display-none');
-    app.classList.add('display-none');
-    showcaseVideo.play();
-  })
-  showcaseVideo.addEventListener('ended', (event) => {
-    showcaseVideo.parentElement.classList.add('display-none');
-    app.classList.remove('display-none');
-  });
+  startShowcaseVideoBtn.addEventListener('click', handleStartShowcaseVideoBtn)
+  showcaseVideo.addEventListener('ended', handleShowcaseVideoEnded);
 } else {
   showcaseVideo.parentElement.classList.add('display-none');
   app.classList.remove('display-none');
