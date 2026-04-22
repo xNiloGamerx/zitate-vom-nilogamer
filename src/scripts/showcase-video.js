335140ea4fb657f975a566ae03b2c01dd9353af5
xNiloGamerx <nikb0322@gmail.com>
2026-04-22 19:47:35 +0200

    Hopfully improved showcase video performance

--- src/scripts/showcase-video.js
+++ src/scripts/showcase-video.js
@@ -1,14 +1,17 @@
 const startShowcaseVideoBtn = document.getElementById('start-showcase-video-btn');
 const showcaseVideo = document.getElementById('showcase-video');
+const app = document.getElementById('app');
 
 if (birthdayMode) {
   showcaseVideo.parentElement.classList.remove('display-none');
 
   startShowcaseVideoBtn.addEventListener('click', (event) => {
     event.target.classList.add('display-none');
+    app.classList.add('display-none');
     showcaseVideo.play();
   })
   showcaseVideo.addEventListener('ended', (event) => {
     showcaseVideo.parentElement.classList.add('display-none');
+    app.classList.remove('display-none');
   });
 }
